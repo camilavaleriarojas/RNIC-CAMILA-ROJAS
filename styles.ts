1dd39f1f25c70b989ebc1cd40f975111e1cd5d03
Camila Rojas <camila.rojas@radiumrocket.com>
2023-07-19 20:02:08 -0300

    RNIC-W03: delete unnecessary code

--- styles.ts
+++ styles.ts
@@ -1,4 +1,4 @@
-import { Platform, StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
 import { darkContent, isAndroid, lightContent } from "./src/constants/themes";
 
 const styles = StyleSheet.create({
